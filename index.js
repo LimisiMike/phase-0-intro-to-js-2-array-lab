7c67bce7b2e2ee5b4806ffb088d563b5b57e1d60
Michael <michaellimisi7@gmail.com>
2023-03-12 12:54:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+// Let's add a reset
+beforeEach(function() {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob");
+    return cats;
+}
+
+function destructivelyRemoveLastCat(Ralph) {
+    cats.pop("Ralph");
+    return cats;
+}
+  
+function destructivelyRemoveFirstCat(Bob) {
+    cats.shift("Bob");
+    return cats;
+}
+
+function appendCat(Broom) {
+    const name = [...cats, "Broom"];
+    return name;
+}
+
+function prependCat(Arnold) {
+    const name = ["Arnold", ...cats];
+    return name;
+}
+
+function removeLastCat() {
+    const Garfield = cats.slice(0, cats.length - 1);
+    return Garfield;
+}
+
+function removeFirstCat() {
+    const Milo = cats.slice(1);
+    return Milo;
+}
